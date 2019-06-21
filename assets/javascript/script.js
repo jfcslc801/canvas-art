d1a3d12e8ebea06b4e49a9153abef43034a69a37
jfrancoc <jfcslc@outlook.com>
2019-06-20 23:10:29 -0600

    hue width and if not drawinf return function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,23 +12,35 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADASS';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 // is drawing var
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 // draw function
 function draw(e) {
+    // stop function when not mouse down
+    if (!isDrawing) return;
     console.log(e);
     ctx.beginPath();
+    // start from
     ctx.moveTo(lastX, lastY);
+    // go to
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 }
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 
 // canvas click and drag even listenters
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
