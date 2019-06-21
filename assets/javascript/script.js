e16e827366988e9086837078ffd4bdaeb1d21384
jfrancoc <jfcslc@outlook.com>
2019-06-20 23:15:49 -0600

    hue increment

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,6 +33,7 @@ function draw(e) {
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
 }
 
 canvas.addEventListener('mousedown', (e) => {
