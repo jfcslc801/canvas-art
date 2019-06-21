a239f703f0c3e15596d26d7c3e90257ebba4c0a7
jfrancoc <jfcslc@outlook.com>
2019-06-20 23:32:48 -0600

    lineWidth direction if else statements

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,13 +12,14 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADASS';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 100;
+ctx.lineWidth = 25;
 
 // is drawing var
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let direction = true;
 
 // draw function
 function draw(e) {
@@ -34,6 +35,18 @@ function draw(e) {
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
     hue++;
+
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
 
 canvas.addEventListener('mousedown', (e) => {
