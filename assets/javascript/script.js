bd9fc25edd3e7edd7b29d8feaffeea3861dd5096
jfrancoc <jfcslc@outlook.com>
2019-06-20 22:46:00 -0600

    canvas click and drag even listenters

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,15 +13,18 @@ ctx.strokeStyle = '#BADASS';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
-// 
+// is drawing var
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
 // draw function
-function draw(e){
+function draw(e) {
     console.log(e);
 }
 
-// canvas mousemove event listener
+// canvas click and drag even listenters
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
